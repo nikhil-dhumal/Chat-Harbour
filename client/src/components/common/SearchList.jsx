454e1847fd0e70d46bce8f7f5929e663303bffac
Nikhil241103 <pekacoclover@gmail.com>
2024-07-17 20:33:10 +0530

    component to display queried usernames

---
+++ client/src/components/common/SearchList.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+
+import { Avatar, Stack, Typography } from "@mui/material"
+
+import userApi from "../../api/modules/user.api"
+import chatApi from "../../api/modules/chat.api"
+
+import { setActiveChat } from "../../redux/features/activeChatSlice"
+
+import getProfileImg from "../../utils/getProfileImg"
+
+const SearchList = ({ searchQuery }) => {
+  const dispatch = useDispatch()
+
+  const [users, setusers] = useState([])
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const { response, err } = await userApi.getUserByName({ username: searchQuery })
+
+      if (response) setusers(response)
+      if (err) setusers([])
+    }
+
+    if (searchQuery.length > 0) getUsers()
+  }, [searchQuery])
+
+  const handleClick = async (id) => {
+    const { response, err } = await chatApi.newChat({ receiverId: id })
+
+    if (response) dispatch(setActiveChat(response))
+    if (err) dispatch(setActiveChat(null))
+  }
+
+  return (
+    <Stack
+      gap={2}
+      sx={{
+        height: "100%",
+        width: "100%",
+        my: 2,
+        overflowY: "auto",
+      }}
+    >
+      {
+        users?.map((user, index) => (
+          <Stack
+            key={index}
+            direction="row"
+            alignItems="center"
+            justifyContent="flex-start"
+            gap={2}
+            sx={{
+              cursor: "pointer"
+            }}
+            onClick={() => handleClick(user.id)}
+          >
+            <Avatar
+              alt={user.username}
+              src={
+                getProfileImg({
+                  gender: user.gender,
+                  username: user.username
+                })
+              }
+            />
+            <Typography>{user.username}</Typography>
+          </Stack>
+        ))
+      }
+    </Stack>
+  )
+}
+
+export default SearchList
