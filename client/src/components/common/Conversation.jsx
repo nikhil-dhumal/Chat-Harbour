4287fafea1e04fa5b01c9cdac1cd07b3acdfd583
Nikhil241103 <pekacoclover@gmail.com>
2024-07-17 20:31:08 +0530

    component for chat details

---
+++ client/src/components/common/Conversation.jsx
@@ -0,0 +1,41 @@
+import { useSelector } from "react-redux"
+
+import { Stack } from "@mui/material"
+
+import MessageInput from "./MessageInput"
+import MessageList from "./MessageList"
+
+const Conversation = () => {
+  const { activeChat } = useSelector((state) => state.activeChat)
+
+  return (
+    <Stack
+      sx={{
+        flex: 4,
+        height: "100%",
+      }}
+    >
+      {
+        activeChat && (
+          <>
+            <Stack
+              gap={2}
+              sx={{
+                px: 2,
+                pt: 1,
+                flexGrow: 1,
+                width: "100%",
+                overflow: "auto"
+              }}
+            >
+              <MessageList />
+            </Stack>
+            <MessageInput />
+          </>
+        )
+      }
+    </Stack>
+  )
+}
+
+export default Conversation
