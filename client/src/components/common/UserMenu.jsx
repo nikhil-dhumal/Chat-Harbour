72268529d7df60152d6a3cd1f71ce9561c7ef015
Nikhil241103 <pekacoclover@gmail.com>
2024-07-17 20:33:47 +0530

    component for user menu

---
+++ client/src/components/common/UserMenu.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
+import { Avatar, Box, ListItemIcon, Menu, MenuItem, Stack, Typography } from "@mui/material"
+import LockResetIcon from "@mui/icons-material/LockReset"
+import LogoutIcon from "@mui/icons-material/Logout"
+
+import { setUser } from "../../redux/features/userSlice"
+
+import getProfileImg from "../../utils/getProfileImg"
+
+const UserMenu = () => {
+  const dispatch = useDispatch()
+
+  const navigate = useNavigate()
+
+  const { user } = useSelector((state) => state.user)
+
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const toggleMenu = (e) => setAnchorEl(e.currentTarget)
+
+  const handleClick = () => {
+    setAnchorEl(null)
+    navigate("/update")
+  }
+
+  return (
+    <Box
+      sx={{
+        height: "100%"
+      }}
+    >
+      <Stack
+        direction="row"
+        alignItems="center"
+        gap={1}
+        sx={{
+          height: "100%",
+          cursor: "pointer",
+          userSelect: "none",
+          position: "relative"
+        }}
+        onClick={toggleMenu}
+      >
+        <Avatar
+          alt={`${user.username}`}
+          src={
+            getProfileImg({
+              gender: user.gender,
+              username: user.username
+            })
+          }
+        />
+        <Typography variant="subtitle1">{user.username}</Typography>
+      </Stack>
+      <Menu
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+      >
+        <MenuItem onClick={handleClick} >
+          <ListItemIcon>
+            <LockResetIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography>Update Password</Typography>
+        </MenuItem>
+        <MenuItem onClick={() => dispatch(setUser(null))} >
+          <ListItemIcon>
+            <LogoutIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography>Sign out</Typography>
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default UserMenu
