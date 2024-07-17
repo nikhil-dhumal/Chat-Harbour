a1445a87bd045fb9405da03a8b5149b62c2f3c25
Nikhil241103 <pekacoclover@gmail.com>
2024-07-17 20:30:32 +0530

    components for chats

---
+++ client/src/components/common/ChatList.jsx
@@ -0,0 +1,99 @@
+import { useDispatch, useSelector } from "react-redux"
+
+import { Avatar, Stack, Typography } from "@mui/material"
+import GroupsIcon from "@mui/icons-material/Groups"
+
+import chatApi from "../../api/modules/chat.api"
+
+import { setActiveChat } from "../../redux/features/activeChatSlice"
+
+import getProfileImg from "../../utils/getProfileImg"
+
+const ChatList = () => {
+  const dispatch = useDispatch()
+
+  const { chats } = useSelector((state) => state.chats)
+
+  const handleClick = async (id) => {
+    const { response, err } = await chatApi.details({ chatId: id })
+
+    if (response) dispatch(setActiveChat(response))
+    if (err) dispatch(setActiveChat(null))
+  }
+
+  return (
+    <Stack
+      gap={2}
+      sx={{
+        height: "100%",
+        width: "100%",
+        my: 2,
+        overflowY: "auto",
+        "&::-webkit-scrollbar": {
+          display: "none"
+        },
+        scrollbarWidth: "none"
+      }}
+    >
+      {
+        chats?.map((chat, index) => (
+          <Stack
+            key={index}
+            direction="row"
+            alignItems="center"
+            justifyContent="flex-start"
+            gap={2}
+            sx={{
+              cursor: "pointer"
+            }}
+            onClick={() => handleClick(chat.id)}
+          >
+            {
+              chat.isGroup
+                ? (
+                  <Avatar><GroupsIcon /></Avatar>
+                ) : (
+                  <Avatar
+                    alt={chat.receiver.username}
+                    src={getProfileImg({ gender: chat.receiver.gender, username: chat.receiver.username })}
+                  />
+                )
+            }
+            <Stack
+              sx={{
+                flexGrow: 1
+              }}
+            >
+              {
+                chat.isGroup
+                  ? (
+                    <Typography>{chat.groupName}</Typography>
+                  ) : (
+                    <Typography>{chat.receiver.username}</Typography>
+                  )
+              }
+              {
+                chat.lastMessage && (
+                  <Typography
+                    variant="caption"
+                    sx={{
+                      display: "-webkit-box",
+                      WebkitBoxOrient: "vertical",
+                      overflow: "hidden",
+                      WebkitLineClamp: 1,
+                      textOverflow: "ellipsis"
+                    }}
+                  >
+                    {chat.lastMessage.content}
+                  </Typography>
+                )
+              }
+            </Stack>
+          </Stack>
+        ))
+      }
+    </Stack >
+  )
+}
+
+export default ChatList
