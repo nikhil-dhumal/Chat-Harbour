d700d0e2ddd34c82bd1b875499aef3a3b8b0c967
Nikhil241103 <pekacoclover@gmail.com>
2024-07-17 20:31:27 +0530

    loader for global loading state

---
+++ client/src/components/common/GlobalLoading.jsx
@@ -0,0 +1,43 @@
+import { useSelector } from "react-redux"
+
+import { Box, Paper } from "@mui/material"
+import { MutatingDots } from "react-loader-spinner"
+
+const GlobalLoading = () => {
+  const { globalLoading } = useSelector((state) => state.globalLoading)
+
+  return (
+    <Paper
+      sx={{
+        opacity: globalLoading ? 1 : 0,
+        pointerEvents: "none",
+        transition: "all .5s ease",
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+        zIndex: 999
+      }}
+    >
+      <Box sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "max-content",
+        height: "max-content"
+      }}>
+        <MutatingDots
+          visible={true}
+          height="100"
+          width="100"
+          color="#23B5D3"
+          secondaryColor="#A755C2"
+          radius="12.5"
+          ariaLabel="mutating-dots-loading"
+        />
+      </Box>
+    </Paper>
+  )
+}
+
+export default GlobalLoading
