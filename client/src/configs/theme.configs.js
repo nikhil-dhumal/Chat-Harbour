6e980af8d91cc26bc0abb526568eb35b48c9764f
Nikhil241103 <pekacoclover@gmail.com>
2024-07-17 20:34:12 +0530

    defined theme

---
+++ client/src/configs/theme.configs.js
@@ -0,0 +1,57 @@
+import { createTheme } from "@mui/material/styles"
+
+export const themeModes = {
+  dark: "dark",
+  light: "light"
+}
+
+const themeConfigs = {
+  custom: ({ mode }) => {
+    const customPalette = mode === themeModes.dark ? {
+      primary: {
+        main: "rgb(35, 181, 211)",
+        contrastText: "rgb(255, 255, 255)"
+      },
+      secondary: {
+        main: "rgb(167, 85, 194)",
+        contrastText: "rgb(255, 255, 255)"
+      },
+      background: {
+        default: "rgb(0, 0, 0)"
+      }
+    } : {
+      primary: {
+        main: "rgb(35, 181, 211)"
+      },
+      secondary: {
+        main: "rgb(167, 85, 194)"
+      },
+      background: {
+        default: "rgb(255, 255, 255)"
+      }
+    }
+
+    return createTheme({
+      palette: {
+        mode,
+        ...customPalette
+      },
+      components: {
+        MuiButton: {
+          defaultProps: {
+            disableRipple: true
+          },
+          styleOverrides: {
+            root: {
+              backgroundImage: "linear-gradient(to right, #23B5D3, #A755C2)",
+              borderRadius: "5px",
+              color: "#ffffff"
+            }
+          }
+        }
+      }
+    })
+  }
+}
+
+export default themeConfigs
