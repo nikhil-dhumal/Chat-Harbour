f1ace2cddbd086f7dc3974603ddd1a9adf99639a
Nikhil241103 <pekacoclover@gmail.com>
2024-07-22 18:38:59 +0530

    add function to add message to a chat

--- client/src/redux/features/chatsSlice.js
+++ client/src/redux/features/chatsSlice.js
@@ -11,13 +11,25 @@ export const chatsSlice = createSlice({
     },
     addChat: (state, action) => {
       state.chats = [action.payload, ...state.chats]
+    },
+    addMessageToChat: (state, action) => {
+      const { chatId, message } = action.payload
+      const chatIndex = state.chats.findIndex(chat => chat.id === chatId)
+
+      if (chatIndex !== -1) {
+        state.chats[chatIndex].messages.push(message)
+        state.chats[chatIndex].lastMessage = message
+        const [updatedChat] = state.chats.splice(chatIndex, 1)
+        state.chats.unshift(updatedChat)
+      }
     }
   }
 })
 
 export const {
   setChats,
-  addChat
+  addChat,
+  addMessageToChat
 } = chatsSlice.actions
 
 export default chatsSlice.reducer
