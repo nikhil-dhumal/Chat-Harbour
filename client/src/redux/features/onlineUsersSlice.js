419a6c6e9e8bd3e977e94c67ecdf2776624571bd
Nikhil241103 <pekacoclover@gmail.com>
2024-07-22 18:38:40 +0530

    improved code readability

--- client/src/redux/features/onlineUsersSlice.js
+++ client/src/redux/features/onlineUsersSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 export const onlineUsersSlice = createSlice({
-  name: "onlineUsers",
+  name: "OnlineUsers",
   initialState: {
     onlineUsers: []
   },
